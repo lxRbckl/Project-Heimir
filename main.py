71693da09d007195bb5a6abfe795c408bb243bf7
highlander <lxrbckl@gmail.com>
2022-11-16 14:26:41 -0600

    vague process

---
+++ main.py
@@ -0,0 +1,27 @@
+# import <
+from github import Github
+from datetime import datetime as dt
+
+# >
+
+
+# local <
+githubToken = ''
+
+gFile = 'data.json'
+gGithub = Github(githubToken)
+gRepository = 'lxRbckl/Project-Heimir'
+gSettingLink = 'https://github.com/lxRbckl/Project-Skotak/raw/main/setting.json'
+
+# >
+
+
+#
+
+
+# main <
+if (__name__ == '__main__'):
+
+    pass
+
+# >
